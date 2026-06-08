d57042e8028e0f723e8c18d9e1f91dbdb5399b46
Faizanzahid-166 <faizanzahid150@gmail.com>
2026-06-08 08:24:16 +0500

    blitz

--- 02_company_site/client/src/components/SEO.jsx
+++ 02_company_site/client/src/components/SEO.jsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 
-const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || 'https://blitztechhub.com';
+const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || '';
 
 export default function SEO({
   title,
